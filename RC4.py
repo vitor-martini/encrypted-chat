4c67e6127d65ebc401ab87b44e4b0a6312d69f0d
Vitor Martini <vitor-martini@outlook.com>
2022-06-07 19:15:29 -0300

    criptografia

--- RC4.py
+++ RC4.py
@@ -2,89 +2,53 @@ import codecs
 
 MOD = 256
 
+def swap(S, i, j):
+    S[i], S[j] = S[j], S[i]
+
 def KSA(key):
-    ''' Key Scheduling Algorithm (from wikipedia):
-        for i from 0 to 255
-            S[i] := i
-        endfor
-        j := 0
-        for i from 0 to 255
-            j := (j + S[i] + key[i mod keylength]) mod 256
-            swap values of S[i] and S[j]
-        endfor
-    '''
     key_length = len(key)
-    # create the array "S"
-    S = list(range(MOD))  # [0,1,2, ... , 255]
+    S = list(range(MOD))  
     j = 0
     for i in range(MOD):
         j = (j + S[i] + key[i % key_length]) % MOD
-        S[i], S[j] = S[j], S[i]  # swap values
+        swap(S, i, j)  
 
     return S
 
-
 def PRGA(S):
-    ''' Psudo Random Generation Algorithm (from wikipedia):
-        i := 0
-        j := 0
-        while GeneratingOutput:
-            i := (i + 1) mod 256
-            j := (j + S[i]) mod 256
-            swap values of S[i] and S[j]
-            K := S[(S[i] + S[j]) mod 256]
-            output K
-        endwhile
-    '''
     i = 0
     j = 0
     while True:
         i = (i + 1) % MOD
         j = (j + S[i]) % MOD
 
-        S[i], S[j] = S[j], S[i]  # swap values
+        swap(S, i, j)  
         K = S[(S[i] + S[j]) % MOD]
         yield K
 
+def encrypt(key, text, decrypt=False):
+    if decrypt == False: text = [ord(byte) for byte in text]
+    key = [ord(byte) for byte in key]    
+    keystream = PRGA(KSA(key))
 
-def get_keystream(key):
-    ''' Takes the encryption key to get the keystream using PRGA
-        return object is a generator
-    '''
-    S = KSA(key)
-    return PRGA(S)
+    cipher_text = []
+    for byte in text:
+        val = ("%02X" % (byte ^ next(keystream)))  
+        cipher_text.append(val)
+    return ''.join(cipher_text)
 
+def decrypt(key, cipher_text):
+    cipher_text = codecs.decode(cipher_text, 'hex_codec')
+    decrypted_text = encrypt(key, cipher_text, True)
+    return codecs.decode(decrypted_text, 'hex_codec').decode('utf-8')
 
-def encrypt_logic(key, text):
-    ''' :key -> encryption key used for encrypting, as hex string
-        :text -> array of unicode values/ byte string to encrpyt/decrypt
-    '''
-    # For plaintext key, use this
-    key = [ord(c) for c in key]
-    # If key is in hex:
-    # key = codecs.decode(key, 'hex_codec')
-    # key = [c for c in key]
-    keystream = get_keystream(key)
+def main():
+    plaintext = input('plaintext: ')
+    key = input('key: ')
+    ecrypted = encrypt(key, plaintext)
+    decrypted = decrypt(key, ecrypted)
 
-    res = []
-    for c in text:
-        val = ("%02X" % (c ^ next(keystream)))  # XOR and taking hex
-        res.append(val)
-    return ''.join(res)
-
-
-def encrypt(key, plaintext):
-    ''' :key -> encryption key used for encrypting, as hex string
-        :plaintext -> plaintext string to encrpyt
-    '''
-    plaintext = [ord(c) for c in plaintext]
-    return encrypt_logic(key, plaintext)
-
-
-def decrypt(key, ciphertext):
-    ''' :key -> encryption key used for encrypting, as hex string
-        :ciphertext -> hex encoded ciphered text using RC4
-    '''
-    ciphertext = codecs.decode(ciphertext, 'hex_codec')
-    res = encrypt_logic(key, ciphertext)
-    return codecs.decode(res, 'hex_codec').decode('utf-8')
+    print('cifrado: ' + ecrypted)
+    print('descifrado: ' + decrypted)
+    
+main()
