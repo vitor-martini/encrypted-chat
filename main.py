c3ec4a2661d0384c65622b666065320d572ec9e3
vitor martini <vitor-martini@outlook.com>
2022-06-07 10:19:08 -0300

    mudança layout

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PySimpleGUI import PySimpleGUI as sg
 import socket
 import threading
 
-def start_server():
+def start_server(IP, chat, window):
     try:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((IP, 3000))
@@ -11,7 +11,7 @@ def start_server():
         server, addr = server_socket.accept()
         while 1:
             data = server.recv(1024)
-            update_chat(data.decode('utf-8'))
+            update_chat(data.decode('utf-8'), chat, window)
             if not data:
                 break
             server.sendall(data)
@@ -19,24 +19,40 @@ def start_server():
     except:
         server.close()
 
-def connect_client(IP):
+def connect_client(client_socket, IP):
     try:
         client_socket.connect((IP, 3000))
         sg.popup('Conexão feita com sucesso!', title = 'Sucesso', icon='padlock_closed.ico')    
     except: 
         sg.popup('Erro de conexão!', title = 'Erro', icon='padlock_closed.ico')
     
-def send_message(message):
+def send_message(client_socket, IP, message, chat, window):
     try:
         message = IP + ': ' + message
-        update_chat(message)
+        update_chat(message, chat, window)
         client_socket.sendall(bytes(message, 'utf-8'))
     except: 
         sg.popup('Erro de conexão!', title = 'Erro', icon='padlock_closed.ico')
     
-def update_chat(message):
+def update_chat(message, chat, window):
     chat.append(message)   
-    janela.find_element('chat').update(values = chat)
+    window['chat'].update(values = chat)
+
+def create_layout(IP, chat):    
+    layout = [
+        [sg.Text('Meu IP: ' + IP)],
+        [sg.Text('IP:'), sg.Input(key='ip', do_not_clear=True, size=(25,25)), sg.Text('Criptografia:'), 
+         sg.Combo(key='combo', values=['S-DES', 'RC4'], enable_events=True), sg.Text('Chave:'), sg.Input(key='key', do_not_clear=True, size=(25,25)), sg.Button('Conectar', key='connect')],
+        [sg.Listbox(key='chat', values = chat, size=(112, 25))],
+        [sg.Input(key='message', do_not_clear=False, size=(106,25)), sg.Button('Enviar', key='send')]
+    ]
+    return layout
+
+def update_key(value, window):
+    if value == 'S-DES':
+        window['key'].update('1000000000')
+    if value == 'RC4':
+        window['key'].update('teste')
 
 def get_current_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -45,27 +61,28 @@ def get_current_ip():
     s.close()
     return IP
     
-def create_layout():    
-    layout = [
-        [sg.Text('Meu IP:' + IP), sg.Text('IP:'), sg.Input(key='ip', do_not_clear=True, size=(25,25)), sg.Button('Conectar', key='conectar')],
-        [sg.Listbox(key='chat', values = chat, size=(112, 25))],
-        [sg.Input(key='mensagem', do_not_clear=False, size=(106,25)), sg.Button('Enviar', key='enviar')]
-    ]
-    return layout
+def main():
+    chat = []
+    IP = get_current_ip()    
 
-chat = []
-IP = get_current_ip()
-server = threading.Thread(target=start_server)
-server.start()
+    # Layout
+    layout = create_layout(IP, chat)
+    window = sg.Window('Encrypted chat', icon='padlock_closed.ico').Layout(layout)    
+    
+    # Conexão
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server = threading.Thread(target=start_server, args=(IP, chat, window,))
+    server.start()
 
-layout = create_layout()
-janela = sg.Window('Encrypted chat', icon='padlock_closed.ico').Layout(layout)
-while True:
-    eventos, valores = janela.read()
-    if eventos == sg.WINDOW_CLOSED:
-        break
-    if eventos == 'conectar':
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connect_client(valores['ip'])
-    if eventos == 'enviar':
-        send_message(valores['mensagem'])
+    while True:
+        events, values = window.read()
+        if events == sg.WINDOW_CLOSED:
+            break
+        if events == 'combo':
+            update_key(values['combo'], window)
+        if events == 'connect':
+            connect_client(client_socket, values['ip'])
+        if events == 'send':
+            send_message(client_socket, IP, values['message'], chat, window)
+
+main()
